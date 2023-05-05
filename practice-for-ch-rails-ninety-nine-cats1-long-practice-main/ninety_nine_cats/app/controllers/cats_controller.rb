0da38775336b8a0bf8f7b67a5e02f10d420dbc36
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-05-05 15:11:48 -0700

    completed the create and new methods

---
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -0,0 +1,27 @@
+class CatsController < ApplicationController
+    def index
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cats_url
+        else
+            render :new
+        end
+    end
+
+    
+end
