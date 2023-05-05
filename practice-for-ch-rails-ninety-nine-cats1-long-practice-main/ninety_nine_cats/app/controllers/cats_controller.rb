3785b2501a3c1180fe88443fea4564310c5dff86
a-wong-8 <awong88@berkeley.edu>
2023-05-05 15:29:20 -0700

    ..

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety_nine_cats/app/controllers/cats_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -23,5 +23,22 @@ class CatsController < ApplicationController
         end
     end
 
-    
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find(params[:id])
+        if @cat.update(cat_params)
+            redirect_to cat_url
+        else
+            render :edit
+        end
+    end
+
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex)
+    end
 end
+
