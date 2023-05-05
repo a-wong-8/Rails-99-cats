d00c18910b0f1e588b68906d236b1d4113eb2daf
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-05-05 14:21:38 -0700

    finished the cats model

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety_nine_cats/app/models/cat.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety_nine_cats/app/models/cat.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Cat < ApplicationRecord
+    CAT_COLORS = ['yellow', 'black', 'white', 'brown', 'orange', 'gray']
+    SEX = ['M', 'F']
+    validates :birth_date, :color, :name, :sex, prescence: true
+    validates :color, inclusion: {in: CAT_COLORS}
+    validates :sex, inclusion: {in: SEX}
+
+
+
 end
